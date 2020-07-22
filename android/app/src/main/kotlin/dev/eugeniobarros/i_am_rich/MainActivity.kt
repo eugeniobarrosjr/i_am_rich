43cd009dc841c8b9e0d6986aa1418c6784b9e96c
Eugênio Barros <eugenio.barros.jr@gmail.com>
2020-07-22 14:54:20 -0300

    Add files

---
+++ android/app/src/main/kotlin/dev/eugeniobarros/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.eugeniobarros.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'I am Rich',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image.asset('images/diamond.png'),
+        ),
+      ),
+    );
+  }
+}
